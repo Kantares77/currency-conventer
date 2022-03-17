570f526b421436994c5f6b9dbace8a2aa9f4bb75
Marcin Kantarski <marcin.kantarski77@gmail.com>
2022-03-17 20:35:14 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+{
+    const finalResult = (enteredAmount, currency) => {
+        const valueEUR = 4.83;
+        const valueGBP = 5.84;
+        const valueUSD = 4.39;
+        const valueCHF = 4.77;
+
+        switch (currency) {
+            case "EUR":
+                return enteredAmount / valueEUR;
+
+            case "GBP":
+                return enteredAmount / valueGBP;
+
+            case "USD":
+                return enteredAmount / valueUSD;
+
+            case "CHF":
+                return enteredAmount / valueCHF;
+        }
+    };
+
+    const updateResultText = (enteredAmount, result, currency) => {
+        const resultElement = document.querySelector(".js-result");
+        resultElement.innerHTML = `Aktualnie za: ${enteredAmount} PLN możesz nabyć: <strong>${result.toFixed(2)} ${currency}</strong>`;
+    }
+
+    const onFormSubmit = (even) => {
+        even.preventDefault();
+
+        const enteredAmountElement = document.querySelector(".js-enteredAmount");
+        const currencyElement = document.querySelector(".js-currency");
+
+        const enteredAmount = enteredAmountElement.value;
+        const currency = currencyElement.value;
+
+        const result = finalResult(enteredAmount, currency);
+
+        updateResultText(enteredAmount, result, currency);
+
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    };
+    init();
+}
